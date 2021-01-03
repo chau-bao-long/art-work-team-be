19059ce65dff4ea78634a9a63dadb7568a9bc12c
long.chau.ext <long.chau.ext@personio.de>
2021-01-03 15:59:33 +0700

    Build Apis for user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/api/user', to: 'user#show'
+  delete '/api/user/:id', to: 'user#delete'
+  post '/api/user', to: 'user#create'
 end
